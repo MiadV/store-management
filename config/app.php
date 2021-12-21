595fa6be4b091b1175a23f914dfef90c03139aea
miadv <miadsoft@gmail.com>
2021-12-21 17:15:07 +0800

    Devtools Clockwork Added

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool)env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -176,6 +176,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Clockwork\Support\Laravel\ClockworkServiceProvider::class,
+
     ],
 
     /*
@@ -230,6 +232,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Clockwork' => Clockwork\Support\Laravel\Facade::class,
 
     ],
 
