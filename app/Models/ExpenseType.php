7bde608925cffbb831de61c00c17bfa407865e39
Miad V <miadsoft@gmail.com>
2021-12-23 00:16:09 +0800

    Shop & ExpenseType Model

---
+++ app/Models/ExpenseType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ExpenseType extends Model
+{
+    use HasFactory;
+}
